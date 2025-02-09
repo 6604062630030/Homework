fe9c66f4af4012a172abeec243f0fb9e8fd1b323
6604062630030 <152254008+6604062630030@users.noreply.github.com>
2025-02-09 23:33:33 +0700

    at_home

---
+++ generateBinary.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void printSol(int *arr, int n) {
+  for (int i = 0; i < n; i++) {
+    cout << arr[i];
+  }
+  cout << endl;
+}
+
+void genBinary(int *arr, int l, int r) {
+  if (l == r) {
+    printSol(arr, r);
+  }
+  else {
+    arr[l] = 0;
+    genBinary(arr, l + 1, r);
+    arr[l] = 1;
+    genBinary(arr, l + 1, r);
+  }
+}
+
+
+int main() {
+  int n = 4;
+  int arr[n];
+
+  genBinary(arr, 0, n);
+}
